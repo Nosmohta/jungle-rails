c90106215e19f03a974176f6bbf6eb65a58cb48d
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-06-09 21:36:58 +0000

    empty cart bug fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,16 @@
 class CategoriesController < ApplicationController
 
-  # def index
-  #   @category = Category.all.order(created_at: :desc)
-  # end
-  #
-  # def create
-  #   # Create query
-  # end
+  def index
+    @category = Category.all.order(created_at: :desc)
+  end
+
+  def show
+    @category = Category.find( params[:id])
+    @products = Product.where(["category_id = ? ", params[:id]])
+  end
+
+  def create
+    # Create query
+  end
 
 end
